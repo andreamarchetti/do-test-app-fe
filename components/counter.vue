0a237c8ddac3b0d359517c279b4b4aedb41be731
Andrea Marchetti <marchetti.andrea@gmail.com>
2020-11-17 17:24:15 +0100

    Nuxt test

---
+++ components/counter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    Ciao {{name}} --- {{teo}} --- {{teodue}}
+    <div @click="add">Add</div>
+    <div @click="clear">Clear</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'Ciccio'
+    }
+  },
+  data () {
+    return { teo: 1 }
+  },
+  computed: {
+    teodue () {
+      return this.teo * 2
+    }
+  },
+  methods: {
+    add () {
+      this.teo += 1
+    },
+    clear () {
+      this.teo = 1
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <counter v-if="cazzo" :name="name" />
+  </div>
+</template>
+<script>
+import counter from '../components/counter'
+
+export default {
+  components: {
+    counter
+  },
+  /*
+  async asyncData () {
+    await ----
+    return null
+  },
+  */
+  data () {
+    return {
+      name: 'ZZZ',
+      cazzaccio: true
+    }
+  },
+  computed: {
+    cazzo () {
+      return this.cazzaccio
+    }
+  },
+  methods: {
+  }
+}
+</script>
